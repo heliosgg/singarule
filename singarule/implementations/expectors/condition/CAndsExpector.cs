6d1a02c5cc48b0920405ca40e940c069caba1c59
Андрей <Andrey22.02@mail.ru>
2020-11-17 21:42:48 +0300

    [feat]: ands expector

--- singarule/implementations/expectors/condition/CAndsExpector.cs
+++ singarule/implementations/expectors/condition/CAndsExpector.cs
@@ -1,6 +1,9 @@
-﻿using singarule.interfaces;
+﻿using singarule.implementations.expressions;
+using singarule.interfaces;
+using singarule.models;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace singarule.implementations.expectors.condition
@@ -9,7 +12,33 @@ namespace singarule.implementations.expectors.condition
    {
       public override bool ExpectIt(ref IWordWalker ww, object currentSingaRule = null)
       {
-         throw new NotImplementedException();
+         if (currentSingaRule is null)
+         {
+            throw new ArgumentNullException();
+         }
+
+         List<string> sigNames = new List<string>();
+
+         var andExpector = new CExactWordExpector("&&");
+         var existingSigNameExpector = new CExistingSigNameExpector();
+         var spaceSkipper = new CWhiteSpaceSkipper();
+
+         do
+         {
+            if (!existingSigNameExpector.ExpectIt(ref ww, currentSingaRule))
+            {
+               error = existingSigNameExpector.error;
+               return false;
+            }
+
+            sigNames.Add(existingSigNameExpector.result);
+            spaceSkipper.ExpectIt(ref ww);
+
+         } while (andExpector.ExpectIt(ref ww) && spaceSkipper.ExpectIt(ref ww));
+
+         result = new CAndExpression();
+         result.Expressions = sigNames.Select(x => new CStateExpression(((SingaRule)currentSingaRule).sigs[x])).ToList<ISingaExpression>();
+         return true;
       }
    }
 }
